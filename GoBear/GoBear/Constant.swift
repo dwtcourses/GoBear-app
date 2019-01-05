1c246f53262b629df5b43f89a97059a4ee365d29
Huy Nguyen <huy.ng1012@gmail.com>
2019-01-06 00:55:26 +0700

    update: constant for GoBear app

--- GoBear/GoBear/Constant.swift
+++ GoBear/GoBear/Constant.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+import UIKit
+
+struct Constant {
+    
+    // MARK: - Storyboard
+    struct Storyboard {
+        
+        static let Main = UIStoryboard(name: "Main", bundle: .main)
+    }
+    
+    // MARK: - Walkthrough Text
+    struct Label {
+        
+        static let Text1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nunc metus, blandit nec porttitor vestibulum, semper eu enim. Praesent quis libero nisi. Proin convallis nibh sed metus scelerisque at euismod erat dignissim. Donec adipiscing magna libero, vestibulum eleifend ipsum. Quisque varius mauris id turpis porttitor convallis. Donec elementum dui a velit congue adipiscing. Etiam sed justo sed lacus condimentum euismod vitae non nisi. Praesent ut ante non leo porttitor placerat eget eu erat.\n\nVestibulum interdum nibh non ante faucibus faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in turpis ipsum. Nullam porta velit nec tellus iaculis ac ultrices quam volutpat. Aenean lobortis imperdiet urna dapibus molestie. Maecenas in urna urna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque vel eros risus, a porttitor nisi."
+        
+        static let Text2 = "Donec id metus vel lorem rutrum mollis. Praesent varius, sem nec malesuada rutrum, nunc velit volutpat tortor, sit amet convallis diam erat ac metus. Donec vel dictum nibh. Vestibulum ultrices nibh sed libero euismod luctus. Aliquam nunc lorem, commodo nec luctus id, varius ac ante. Morbi nec ipsum eu sapien cursus condimentum eu in mi. \n\nInteger posuere dolor sed augue accumsan molestie. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. \n\nCurabitur eu dolor libero, eget sollicitudin lorem. Etiam malesuada pharetra eleifend. Integer eget adipiscing ipsum. Ut eu egestas nisl. In hac habitasse platea dictumst. Phasellus eu eros sit amet mauris scelerisque tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nCras condimentum turpis vel risus tincidunt eu tristique erat dignissim. Vivamus gravida quam sit amet ante cursus auctor. Aliquam nulla neque, porta sed volutpat id."
+        
+        static let Text3 = "Ut nisl urna, imperdiet id consectetur sed, pellentesque nec magna. Donec congue rhoncus diam, ut sodales enim molestie eget. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In vel orci at quam pellentesque tempus. Cras vulputate sollicitudin quam, commodo porttitor felis auctor nec. \n \nMaecenas pellentesque scelerisque nulla, hendrerit dignissim dolor tincidunt venenatis. Praesent eget sapien vel quam vestibulum aliquam a nec lectus. Praesent pretium ultrices semper. Aenean sem lectus, semper vel laoreet eget, rhoncus et orci.\n \nQuisque accumsan dui at nisi gravida ornare. Donec fermentum, sapien vel tincidunt ultrices, diam ligula ullamcorper purus, eu imperdiet nisl ante eu purus. Nullam elit nisl, commodo ut lacinia vel, tincidunt sit amet ligula. Proin purus tortor, suscipit fringilla hendrerit nec, eleifend at mi. Vestibulum ultrices nulla eget lorem dictum eget vehicula dolor iaculis. Suspendisse potenti."
+    }
+}
