4175e7568403e59127731c2b20a53a668c9db972
Huy Nguyen <huy.ng1012@gmail.com>
2019-01-06 22:39:05 +0700

    update: keyboard manager

--- GoBear/GoBear/KeyboardObserveViewController.swift
+++ GoBear/GoBear/KeyboardObserveViewController.swift
@@ -7,24 +7,72 @@
 //
 
 import UIKit
+import RxCocoa
+import GoBearCore
 
-class KeyboardObserveViewController: UIViewController {
+class KeyboardObserveViewController: BaseViewController {
 
+    //    MARK:- OUTLETS
+    @IBOutlet weak var contentScrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    //    MARK:- Variables
+    lazy var keyboardManager: KeyboardManager? = {
+        if let _ = self.contentScrollView {
+            return KeyboardManager(scrollView: self.contentScrollView)
+        }
+        return nil
+    }()
+    
+    //    MARK:- View Controller Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        binding()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        keyboardManager?.beginObservingKeyboard()
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        keyboardManager?.endObservingKeyboard()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //  MARK:- Public Functions
+    public func setShowUpViewBelowKeyboardBlockForActiveEditingField(_ editingField: UIView) {
+        keyboardManager?.showUpViewBelowKeyboardHandler = { [weak self] keyboardHeight in
+            guard let strongSelf = self else { return }
+            var aRect = strongSelf.view.frame
+            aRect.size.height -= UIScreen.main.bounds.height/2
+            let textFieldFrame = strongSelf.contentView.convert(editingField.frame, from: editingField.superview)
+            if !aRect.contains(textFieldFrame) {
+                let contentOffset = CGPoint(x: 0, y: textFieldFrame.origin.y - aRect.height)
+                strongSelf.contentScrollView.setContentOffset(contentOffset, animated: false)
+            }
+        }
+    }
+}
 
+//      MARK: Private
+extension KeyboardObserveViewController {
+    
+    fileprivate func binding() {
+        
+        // Touch gesture for view
+        let touchGesture = UITapGestureRecognizer()
+        self.view.addGestureRecognizer(touchGesture)
+        
+        touchGesture.rx
+            .event
+            .bind { recognizer in
+                self.view.endEditing(true)
+            }.disposed(by: disposeBag)
+    }
 }
